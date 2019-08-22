12fe36103a561bb814dd76a5189e979a492aec64
olshkol <olegskolnikov@gmail.com>
2019-08-22 21:51:38 +0300

    add admin panel and user roles

--- src/main/java/com/olshkol/sweater/controller/MainController.java
+++ src/main/java/com/olshkol/sweater/controller/MainController.java
@@ -27,7 +27,7 @@ public class MainController {
 
     @GetMapping("/main")
     public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
-        Iterable<Message> messages = messageRepository.findAll();
+        Iterable<Message> messages;
         if (filter != null && !filter.isBlank())
             messages = messageRepository.findByTag(filter);
         else messages = messageRepository.findAll();
