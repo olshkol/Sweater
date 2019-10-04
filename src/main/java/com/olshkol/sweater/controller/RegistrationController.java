28da8d7424b779835db365abe749fc62774d0bdd
olshkol <olegskolnikov@gmail.com>
2019-10-04 20:22:18 +0300

    reCaptcha, rest client, rememberMe and saving sessions in db

--- src/main/java/com/olshkol/sweater/controller/RegistrationController.java
+++ src/main/java/com/olshkol/sweater/controller/RegistrationController.java
@@ -1,25 +1,39 @@
 package com.olshkol.sweater.controller;
 
 import com.olshkol.sweater.domain.User;
+import com.olshkol.sweater.domain.dto.CaptchaResponseDto;
 import com.olshkol.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
+    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
     private final UserService userService;
 
+    private final RestTemplate restTemplate;
+
+    @Value("${recaptcha.secret}")
+    private String secret;
+
     @Autowired
-    public RegistrationController(UserService userService) {
+    public RegistrationController(UserService userService, RestTemplate restTemplate) {
         this.userService = userService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/registration")
@@ -28,11 +42,28 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model){
-        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())){
+    public String addUser(
+            @RequestParam("password2") String passwordConfirm,
+            @RequestParam("g-recaptcha-response") String captchaResponse,
+            @Valid User user,
+            BindingResult bindingResult,
+            Model model)
+    {
+        String url = String.format(CAPTCHA_URL, secret, captchaResponse);
+        CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if (!response.isSuccess()){
+            model.addAttribute("captchaError", "Fill captcha");
+        }
+
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+        if (isConfirmEmpty){
+            model.addAttribute("password2Error","Password confirmation cannot be empty");
+        }
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirm)){
             model.addAttribute("passwordError", "Password are different!");
         }
-        if (bindingResult.hasErrors()){
+        if (isConfirmEmpty || bindingResult.hasErrors() || !response.isSuccess()){
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
             model.mergeAttributes(errors);
             return "registration";
@@ -48,9 +79,14 @@ public class RegistrationController {
     public String activate(Model model, @PathVariable String code){
         boolean isActivated = userService.activateUser(code);
 
-        if (isActivated) model.addAttribute("message", "User successfully activated");
-        else model.addAttribute("message", "Activation code is not found");
-
+        if (isActivated) {
+            model.addAttribute("messageType", "success");
+            model.addAttribute("message", "User successfully activated");
+        }
+        else {
+            model.addAttribute("messageType", "danger");
+            model.addAttribute("message", "Activation code is not found");
+        }
         return "login";
     }
 
