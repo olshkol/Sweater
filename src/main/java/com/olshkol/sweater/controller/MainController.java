ca4ba73d5f593a1024cffb75fed0da1aefa3cdfe
olshkol <olegskolnikov@gmail.com>
2019-10-08 20:19:22 +0300

    pagination

--- src/main/java/com/olshkol/sweater/controller/MainController.java
+++ src/main/java/com/olshkol/sweater/controller/MainController.java
@@ -5,6 +5,10 @@ import com.olshkol.sweater.domain.User;
 import com.olshkol.sweater.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -41,13 +45,18 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
-        Iterable<Message> messages;
+    public String main(
+            @RequestParam(required = false, defaultValue = "") String filter,
+            Model model,
+            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        Page<Message> page;
         if (filter != null && !filter.isBlank())
-            messages = messageRepository.findByTag(filter);
-        else messages = messageRepository.findAll();
+            page = messageRepository.findByTag(filter, pageable);
+        else page = messageRepository.findAll(pageable);
 
-        model.addAttribute("messages", messages);
+        model.addAttribute("page", page);
+        model.addAttribute("url", "/main");
         model.addAttribute("filter", filter);
         return "main";
     }
